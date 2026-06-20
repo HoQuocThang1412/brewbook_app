f994ca052b1b3ba940417398d5ca8bb14b7a546f
HoQuThang <124477233+HoQuocThang1412@users.noreply.github.com>
2026-06-20 18:57:11 +0700

    Seed Excel recipes on app start

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+
 import 'services/hive_service.dart';
+import 'services/imported_recipes.dart';
 import 'theme/app_theme.dart';
 import 'screens/main_navigation_screen.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await HiveService.init();
+  await _seedExcelRecipesOnce();
   runApp(const BrewBookApp());
+}
+
+Future<void> _seedExcelRecipesOnce() async {
+  final settingsBox = await Hive.openBox('settings_box');
+  final alreadySeeded = settingsBox.get(excelRecipesSeedVersion) == true;
+
+  if (alreadySeeded) return;
+
+  await seedImportedRecipes(HiveService.box);
+  await settingsBox.put(excelRecipesSeedVersion, true);
 }
 
 class BrewBookApp extends StatelessWidget {
