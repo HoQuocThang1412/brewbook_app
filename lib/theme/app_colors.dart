c4904eab4a0509d5453679deb80095362d35b6aa
HoQuThang <124477233+HoQuocThang1412@users.noreply.github.com>
2026-06-20 18:33:19 +0700

    Switch to clean light palette

--- lib/theme/app_colors.dart
+++ lib/theme/app_colors.dart
@@ -3,27 +3,27 @@ import 'package:flutter/material.dart';
 class AppColors {
   AppColors._();
 
-  static const Color background = Color(0xFF0F1111);
-  static const Color surface = Color(0xFF1B1E1E);
-  static const Color surfaceAlt = Color(0xFF262A2A);
-  static const Color card = Color(0xFF242828);
+  static const Color background = Color(0xFFF7F3EC);
+  static const Color surface = Color(0xFFFFFFFF);
+  static const Color surfaceAlt = Color(0xFFF1E9DC);
+  static const Color card = Color(0xFFFFFFFF);
 
-  static const Color coffeeDark = Color(0xFFF7F5F0);
-  static const Color coffeeBrown = Color(0xFFD8A15D);
-  static const Color coffeeLight = Color(0xFFB9B1A5);
+  static const Color coffeeDark = Color(0xFF2D241D);
+  static const Color coffeeBrown = Color(0xFF6E4B32);
+  static const Color coffeeLight = Color(0xFFC8A47E);
 
-  static const Color gold = Color(0xFFE3B86A);
-  static const Color goldSoft = Color(0xFF332A1E);
+  static const Color gold = Color(0xFFD7A13E);
+  static const Color goldSoft = Color(0xFFFFF3D7);
 
-  static const Color textPrimary = Color(0xFFF7F5F0);
-  static const Color textSecondary = Color(0xFFB9B9B3);
-  static const Color textOnDark = Color(0xFFF7F5F0);
+  static const Color textPrimary = Color(0xFF2D241D);
+  static const Color textSecondary = Color(0xFF7A6B5C);
+  static const Color textOnDark = Color(0xFFFFFFFF);
 
-  static const Color success = Color(0xFF74D68B);
-  static const Color successSoft = Color(0xFF173522);
-  static const Color danger = Color(0xFFFF7A66);
-  static const Color dangerSoft = Color(0xFF3A1E1B);
+  static const Color success = Color(0xFF3F8F5B);
+  static const Color successSoft = Color(0xFFEAF5EE);
+  static const Color danger = Color(0xFFC75D4A);
+  static const Color dangerSoft = Color(0xFFF9E7E2);
 
-  static const Color border = Color(0xFF343938);
-  static const Color shadow = Color(0x55000000);
+  static const Color border = Color(0xFFE7DDD0);
+  static const Color shadow = Color(0x14000000);
 }
