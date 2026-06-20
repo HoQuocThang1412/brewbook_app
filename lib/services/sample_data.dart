4f058a21b6ab47f64d2bbb45085eec04df318145
HoQuThang <124477233+HoQuocThang1412@users.noreply.github.com>
2026-06-20 23:38:33 +0700

    Seed recovered test recipes for fresh installs

--- lib/services/sample_data.dart
+++ lib/services/sample_data.dart
@@ -2,7 +2,9 @@ import 'package:hive/hive.dart';
 
 import '../models/recipe.dart';
 import 'imported_recipes.dart';
+import 'recovered_test_recipe_seed.dart';
 
 Future<void> seedSampleData(Box<Recipe> box) async {
   await seedImportedRecipes(box);
+  await seedRecoveredTestRecipes(box);
 }
