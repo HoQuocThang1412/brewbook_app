24bfa5705a4554cced9bb17c70116971bed30166
HoQuThang <124477233+HoQuocThang1412@users.noreply.github.com>
2026-06-20 23:49:55 +0700

    Restore default recipe seed

--- lib/services/sample_data.dart
+++ lib/services/sample_data.dart
@@ -2,9 +2,7 @@ import 'package:hive/hive.dart';
 
 import '../models/recipe.dart';
 import 'imported_recipes.dart';
-import 'recovered_test_recipe_seed.dart';
 
 Future<void> seedSampleData(Box<Recipe> box) async {
   await seedImportedRecipes(box);
-  await seedRecoveredTestRecipes(box);
 }
